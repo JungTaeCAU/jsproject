b49212855bb7bb32502bc31075e0cfc903721152
JungTaeCAU <ddee1212@naver.com>
2022-04-29 09:34:43 +0900

    mouse event

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
+ const title = document.querySelector(".hello h1");
+
+ title.innerText = "Hello"
+
+ console.log(title);
+
+ function handelTitleClick(){
+     title.style.color = "blue";
+ }
  
+ function handelMouseEnter(){
+    title.innerText = "mouse is here";
+ }
+
+function handelMouseLeave(){
+    title.innerText = "mouse is gone";
+}
+
+ title.addEventListener("click", handelTitleClick)
+ title.addEventListener("mouseenter", handelMouseEnter)
+ title.addEventListener("mouseleave", handelMouseLeave)
